d6e851c0577a774ba482edf89a367cdd1ec885f8
박경일 <baggyeong-il@baggyeong-il-ui-MacBookPro.local>
2024-04-06 22:16:14 +0900

    server side fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,17 @@
-"use client"
+export const metadata = {
+  title: "Home",
+}
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
 
-import { useEffect, useState } from "react";
+async function getMovies() {
+  return fetch(URL).then(res => res.json());
+}
 
-export default function Page() {
-  const [isLoading, setIsLoadind] = useState(true);
-  const [moives, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    const json = await response.json();
-    setMovies(json);
-    setIsLoadind(false);
-  }
-  useEffect(() => {
-    getMovies();
-  },[]);
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      {isLoading ? "Loading..." : JSON.stringify(moives)}
+      {JSON.stringify(movies)}
     </div>
   );
 }
