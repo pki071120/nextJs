c86e4acd546edbbbb3adb7830d539ed3a7ef3f8a
pki071120 <kollagajoha@gmail.com>
2024-04-11 14:47:08 +0900

    movie trailers

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -13,8 +13,8 @@ export default async function MovieDetail({params: {
     <Suspense fallback={<h1>Loading movie info</h1>}>
       <MovieInfo id={id}/>
     </Suspense>
-    {/* <Suspense fallback={<h1>Loading movie videos</h1>}>
-      <MovieVideos id={id}/>
-    </Suspense> */}
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id}/>
+      </Suspense>
   </div>)
 }
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -11,9 +11,14 @@ export default async function MovieInfo({id}: {id:string}){
   return <div className={styles.container}>
   <img src={info.poster_path} alt="경로 못찾음" className={styles.poster}/>
   <div className={styles.detail}>
-    <h2>{info.title}</h2>
-    <h2>{info.vote_average}</h2>
-    <p>{info.overview}</p>
+    <h2 className={styles.title}>{info.title}</h2>
+    <div className={styles.genresContainer}>
+      genres: 
+      {info.genres.map((genre) => <span key={genre.id} className={styles.genre}> {genre.name},</span>)}
+    </div>
+    <h2 className={styles.runtime}>Runtime : {Math.floor(info.runtime/60)}H {info.runtime%60 == 0 ?null : info.runtime%60+"M"}</h2>
+    <h2 className={styles.reviewpoint}>⭐️ {info.vote_average}</h2>
+    <p className={styles.overview}>{info.overview}</p>
   </div>
   </div>
 }
